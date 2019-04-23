2278afbe424c274975755a3e509e154f1a646173
Guido Vranken <guidovranken@gmail.com>
2019-04-23 17:16:20 +0200

    MemorySanitizer support

--- fuzzer.cpp
+++ fuzzer.cpp
@@ -6,6 +6,12 @@
 #include <rapidjson/document.h>
 #include <rapidjson/prettywriter.h>
 
+#ifdef MSAN
+extern "C" {
+    void __msan_check_mem_is_initialized(const volatile void *x, size_t size);
+}
+#endif
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     const std::string s(data, data + size);
@@ -21,6 +27,12 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     rapidjson::StringBuffer sb;
     rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(sb);
     document.Accept(writer);
+    str = sb.GetString();
+#ifdef MSAN
+    if ( str.size() ) {
+        __msan_check_mem_is_initialized(str.data(), str.size());
+    }
+#endif
 
     return 0;
 }
